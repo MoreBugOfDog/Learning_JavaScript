175fdfda7fcbc2b1b4112f8e413db2e96c8fc4c2
多bug的啸天犬 <2872332842@qq.com>
2024-04-07 09:50:59 +0000

    update

--- image_archive/gallery-start/main.js
+++ image_archive/gallery-start/main.js
@@ -29,7 +29,6 @@ for (const image of images) {
 }
 
 /* Wiring up the Darken/Lighten button */
-
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
   if (btnClass === 'dark') {
